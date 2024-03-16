f709bf838b7f173b8f664560a34083b36b443ca6
vuvuzelich <louielamani@ukr.net>
2024-03-16 13:58:01 +0200

    Initial Commit

---
+++ RealmTestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RealmTestApp
+//
+//  Created by Макс Лахман on 16.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RealmTestApp/RealmTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmTestAppApp.swift
+//  RealmTestApp
+//
+//  Created by Макс Лахман on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct RealmTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
